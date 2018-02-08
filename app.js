74f1fb313e0b33a15a5ca57c9e53147b29fb83d9
Joyce <joycelamsaungan@gmail.com>
2018-02-07 17:02:38 -0800

    adding quick search function

--- app.js
+++ app.js
@@ -20,6 +20,55 @@ var location;
 var topic;
 var price;
 
+
+$("#quick-search").on("click", function() {
+    findCurrentLocation();
+    $("#map-space").hide();
+});
+
+$("#loc-search").on("click", function() {
+    $("#map-space").show();
+    initMap();
+})
+
+$(document).ready(function() {
+    $("#map-space").hide();
+} )
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+
+function findCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            console.log(pos);
+            console.log(pos.lat);
+            console.log(pos.lng);
+            quickSearch(pos.lat, pos.lng);
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
 function initMap() {
     // Initialize map to San Francisco
     var sanFrancisco = { lat: 37.773972, lng: -122.431297 };
@@ -29,27 +78,21 @@ function initMap() {
         mapTypeId: "roadmap"
     });
     infoWindow = new google.maps.InfoWindow;
-
     // Find current location and center the map there
     // findCurrentLocation();
-
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
-
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
         searchBox.setBounds(map.getBounds());
     });
-
     var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     searchBox.addListener('places_changed', function() {
         var places = searchBox.getPlaces();
-
         if (places.length == 0) {
             return;
         }
@@ -57,12 +100,9 @@ function initMap() {
         markers.forEach(function(marker) {
             marker.setMap(null);
         });
-
         markers = [];
-
         // For each place, get the icon, name and location.
         var bounds = new google.maps.LatLngBounds();
-
         places.forEach(function(place) {
             if (!place.geometry) {
                 console.log("Returned place contains no geometry");
@@ -75,7 +115,6 @@ function initMap() {
                 anchor: new google.maps.Point(17, 34),
                 scaledSize: new google.maps.Size(25, 25)
             };
-
             // Create a marker for each place.
             var marker = new google.maps.Marker({
                 map: map,
@@ -83,16 +122,13 @@ function initMap() {
                 title: place.name,
                 position: place.geometry.location
             });
-
             markers.push(marker);
-
             var infowindow = new google.maps.InfoWindow();
             google.maps.event.addListener(marker, 'click', function() {
                 infowindow.setContent(getInfoContent(place));
                 console.log(place);
                 infowindow.open(map, this);
             });
-
             if (place.geometry.viewport) {
                 // Only geocodes have viewport.
                 bounds.union(place.geometry.viewport);
@@ -100,19 +136,9 @@ function initMap() {
                 bounds.extend(place.geometry.location);
             }
         });
-
         map.fitBounds(bounds);
-        location = $("#pac-input").val().trim();
-        console.log($("#pac-input").val());
-
-        database.ref("/locations").push({
-            location: location
-        })
-
-    })
-};
-
-
+    });
+}
 function getInfoContent(place) {
     var isOpen;
     if (place.opening_hours.open_now) {
@@ -120,9 +146,7 @@ function getInfoContent(place) {
     } else {
         isOpen = "Closed";
     }
-
     var openHours = place.opening_hours.weekday_text;
-
     return '<div><strong>' + place.name + '</strong><br>' +
         place.formatted_address + '<br>' +
         isOpen + '<br>' +
@@ -132,18 +156,32 @@ function getInfoContent(place) {
 }
 
 
+
 //Eventbrite
 
 
-function evenbriteSearch(){     //Requesting info and adding the value of the button to the url
-    //https://www.eventbriteapi.com/v3/events/search/?location.address=san+francisco&location.within=30mi&token=KJSHU43DGDL7JI6OFUYJ
-    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q="+topic+"&sort_by=date&location.address="+location+"&location.within="+range+"mi&price="+price+"&token=KJSHU43DGDL7JI6OFUYJ";
+// This function will be called after pressing Quick Search bttn, it will use latitude and longitude from Maps API
+function quickSearch(latitude, longitude){ 
+
+    //https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.latitude=37.784373&location.longitude=-122.407705&token=KJSHU43DGDL7JI6OFUYJ
+    var latitudeLongitude = "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.latitude="+latitude+"&location.longitude="+longitude+"&token=KJSHU43DGDL7JI6OFUYJ";
+    $.ajax({
+    url: latitudeLongitude,
+    method: "GET"
+  })
+  .then(function(response) {
+        console.log(response);
+    });
+}
+
+function evenbriteSearch(){     //Requesting info and adding the value of every button to the url
+
+    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q="+topic+"&sort_by=date&location.address="+location+"&location.within="+range+"&price="+price+"&token=KJSHU43DGDL7JI6OFUYJ";
     $.ajax({
     url: queryURL,
     method: "GET"
   })
   .then(function(response) {
-        
         console.log(response);
     });
 }
@@ -162,7 +200,7 @@ $(".topic").on("click",function(){
 var range="";
 
 $(".range").on("click",function(){
-    topic=$(this).val();
+    range=$(this).val()+"mi";
     console.log(range)
 
     database.ref("/range").push({
@@ -179,8 +217,7 @@ $(".price").on("click",function(){
     database.ref("/price").push({
         price: price
     })
-
-    evenbriteSearch();
 })
+
 
 
